98e0f54d0174c58f793031b8bf68900f2062d907
Attit Maharjan <mhrjn.attit@gmail.com>
2025-02-19 14:11:26 +0100

    Added CRUD functionality for tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list, task_create, task_update, task_delete
 
 urlpatterns = [
     path('', task_list, name='task-list'),
+    path('create/', task_create, name='task-create'),
+    path('update/<int:task_id>/', task_update, name='task-update'),
+    path('delete/<int:task_id>/', task_delete, name='task-delete'),
 ]
